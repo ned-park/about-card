83e7fb35132692e7c5c548c332ff705918ecc708
ned-park <87483870+ned-park@users.noreply.github.com>
2023-01-25 15:47:31 -0500

    feat: Add Interests component

---
+++ src/components/Interests.jsx
@@ -0,0 +1,10 @@
+export default function Interests() {
+  return (
+    <div className="interests">
+      <h2 className="interests--header">Interests</h2>
+      <p className="interests--p">
+        lorem ipsum a;sldkjf;laskdjf ;lakjsdflkj alkjsd kjasdflkj alsdjf lkjsadfkkkkasdf asdf asdlfkjhasdlkfjhasdklf jalksdf.
+      </p>
+    </div>
+  )
+}
