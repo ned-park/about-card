0d401f96aa54913bea253730606680808a4f0e2f
ned-park <87483870+ned-park@users.noreply.github.com>
2023-01-25 15:47:47 -0500

    feat: Add Footer component

---
+++ src/components/Footer.jsx
@@ -0,0 +1,18 @@
+export default function Footer() {
+  return (
+    <footer className="footer">
+      <a href="https://twitter.com/park_ned">
+        <i class="fa-brands fa-square-twitter"></i>
+      </a>
+      <a href="https://github.com/ned-park">
+        <i class="fa-brands fa-square-github"></i>
+      </a>
+      <a href="mailto:nedpark_dev@protonmail.com">
+        <i class="fa-solid fa-square-envelope"></i>
+      </a>
+      <a href="https://linkedin.com/in/ned-park">
+        <i class="fa-brands fa-linkedin"></i>
+      </a>
+    </footer>
+  );
+}
