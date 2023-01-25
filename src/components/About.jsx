496805f2de3cbe612459ec997f0382bab7d20d04
ned-park <87483870+ned-park@users.noreply.github.com>
2023-01-25 15:46:47 -0500

    feat: Add about component

---
+++ src/components/About.jsx
@@ -0,0 +1,10 @@
+export default function About() {
+  return (
+    <div className="about">
+      <h2 className="about--header">About</h2>
+      <p className="about--p">
+        lorem ipsum a;sldkjf;laskdjf ;lakjsdflkj alkjsd kjasdflkj alsdjf lkjsadfkkkkasdf asdf asdlfkjhasdlkfjhasdklf jalksdf.
+      </p>
+    </div>
+  )
+}
